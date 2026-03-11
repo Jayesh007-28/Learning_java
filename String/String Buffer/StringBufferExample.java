fe4ea00f160936ab02b49da80e4d8791e503b000
Jayesh <jayeshajay2003@gmail.com>
2026-03-11 09:17:33 +0530

    String buffer

---
+++ String/String Buffer/StringBufferExample.java
@@ -0,0 +1,10 @@
+public class StringBufferExample {
+    public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer("Jayesh");
+
+        sb.append(" Java");
+
+        System.out.println(sb);
+    }
+}
