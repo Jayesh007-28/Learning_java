c1c582ade18e86ffc261270cd66a3de5c0272f7b
Jayesh <jayeshajay2003@gmail.com>
2026-03-12 13:15:09 +0530

    Throw

---
+++ Exception Handling/ThrowExample.java
@@ -0,0 +1,12 @@
+public class ThrowExample {
+    public static void main(String[] args) {
+        
+        int age = 16;
+        if(age<18) {
+            throw new ArithmeticException("You are not eligible");
+        }
+
+        System.out.println("You are eligible");
+
+    }
+}
