601fcb3da14ff3d5ecf945c5e011ea4d05037c62
Jayesh <jayeshajay2003@gmail.com>
2026-03-12 08:40:12 +0530

    Try block

---
+++ Exception Handling/TryExample.java
@@ -0,0 +1,14 @@
+public class TryExample {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a/b;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by ZERO.......");
+        }
+
+        System.out.println("Program continues......");
+    }
+}
