eb31de508e908f4306770eacbaf7758c99b08ad5
Jayesh <jayeshajay2003@gmail.com>
2026-03-13 19:12:04 +0530

    Using abstract class

--- Inner Class/AnonymsInnerClass.java
+++ Inner Class/AnonymsInnerClass.java
@@ -1,16 +1,32 @@
 
-interface Greeting {
-    void sayHello();
+// interface Greeting {
+//     void sayHello();
+// }
+
+// public class AnonymsInnerClass {
+//     public static void main(String[] args) {
+//         Greeting obj = new Greeting() {
+//             public void sayHello() {
+//                 System.out.println("Hello Jayesh...");
+//             }
+//         };
+
+//         obj.sayHello();
+//     }
+// }
+
+abstract class Greeting {
+    abstract void display();
 }
 
 public class AnonymsInnerClass {
     public static void main(String[] args) {
         Greeting obj = new Greeting() {
-            public void sayHello() {
-                System.out.println("Hello Jayesh...");
+            public void display(){
+                System.out.println("Hi Jayesh...");
             }
         };
 
-        obj.sayHello();
+        obj.display();
     }
 }
